d6eacd378f4c960575d5a04f78bcce6056b0ba97
Henrique Viegas <dagostiniviegas@gmail.com>
2020-11-14 23:41:00 -0300

    backend da aplicação com atualização em banco de dados

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,56 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
 
+import AppError from '../errors/AppError';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoryRepository = getRepository(Category);
+
+    const { total } = await transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new AppError('Total cash not suficient for transaction');
+    }
+
+    let checkCategoryExists = await categoryRepository.findOne({
+      where: { category },
+    });
+
+    if (!checkCategoryExists) {
+      checkCategoryExists = categoryRepository.create({
+        title: category,
+      });
+
+      await categoryRepository.save(checkCategoryExists);
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category: checkCategoryExists,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
